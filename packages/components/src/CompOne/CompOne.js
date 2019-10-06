4f36733585d03ac2cbc559f4ae1705e462d9cd70
Eugene Samonenko <esamonenko@agilevision.io>
2019-10-06 10:37:55 +0300

    init from react-workspaces-playground repo

---
+++ packages/components/src/CompOne/CompOne.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CompOne.css";
+
+const CompOne = () => (
+  <div className="Comp">
+    <h3>
+      <span role="img" aria-label="React Logo">
+        ⚛️
+      </span>
+        Ambroseus 1
+    </h3>
+  </div>
+);
+
+export default CompOne;
---
+++ packages/components/src/CompTwo/CompTwo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CompTwo.css";
+
+const CompTwo = () => (
+  <div className="Comp">
+    <h3>
+      <span role="img" aria-label="Yarn Logo">
+        🐱
+      </span>{" "}
+      Ambroseus 2
+    </h3>
+  </div>
+);
+
+export default CompTwo;
